1de76bc417e8dc7018f52ad294ea42c0b0cefaf3
careydevelopment <mrbrianmcarey@gmail.com>
2021-10-13 04:32:50 -0400

    get rid of debug code

--- src/main/java/com/careydevelopment/ecosystem/user/UserApplication.java
+++ src/main/java/com/careydevelopment/ecosystem/user/UserApplication.java
@@ -3,11 +3,10 @@ package com.careydevelopment.ecosystem.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication(scanBasePackages = {"us.careydevelopment.ecosystem", "com.careydevelopment.ecosystem.user"})
+@SpringBootApplication(scanBasePackages = { "us.careydevelopment.ecosystem", "com.careydevelopment.ecosystem.user" })
 public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class,args);
+        SpringApplication.run(UserApplication.class, args);
     }
 }
